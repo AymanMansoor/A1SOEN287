4aaa1b48f977b2267cec19ea4121d52481fe6ae8
CarlaShawi <91223318+CarlaShawi@users.noreply.github.com>
2021-11-06 14:00:16 -0400

    Update P4cart-script.js

--- P4cart-script.js
+++ P4cart-script.js
@@ -8,32 +8,32 @@ function onClick(){
     var tempPrice = 0;
 
     function calculatePriceBeef() {
-        var priceBeef = (totalProductInCart[0].value)*10.99;
+        var priceBeef = (totalProductInCart[0].value)*10.99
         console.log(priceBeef);
         tempPrice += priceBeef;
         console.log(tempPrice);
-        document.getElementById("temp-price-display").innerHTML = "$" + tempPrice;
-        document.getElementById("qst-price-display").innerHTML = "$" + tempPrice*QST;
-        document.getElementById("gst-price-display").innerHTML = "$" + tempPrice*GST;
-        document.getElementById("total-price-display").innerHTML = "$" + (tempPrice + (tempPrice*QST) + (tempPrice*GST));
+        document.getElementById("temp-price-display").innerHTML =  "$" + (tempPrice ).toFixed(2);
+        document.getElementById("qst-price-display").innerHTML = "$" + (tempPrice*QST).toFixed(2);
+        document.getElementById("gst-price-display").innerHTML = "$" + (tempPrice*GST).toFixed(2);
+        document.getElementById("total-price-display").innerHTML = "$" + ((tempPrice + (tempPrice*QST) + (tempPrice*GST))).toFixed(2);
     }
     
     function calculatePriceTikka(){
         var priceTikka = (totalProductInCart[1].value)*10.99
         tempPrice += priceTikka;
-        document.getElementById("temp-price-display").innerHTML = "$" + tempPrice;
-        document.getElementById("qst-price-display").innerHTML = "$" + tempPrice*QST;
-        document.getElementById("gst-price-display").innerHTML = "$" + tempPrice*GST;
-        document.getElementById("total-price-display").innerHTML = "$" + (tempPrice + (tempPrice*QST) + (tempPrice*GST));
+        document.getElementById("temp-price-display").innerHTML = "$" + (tempPrice).toFixed(2);
+        document.getElementById("qst-price-display").innerHTML = "$" + (tempPrice*QST).toFixed(2);
+        document.getElementById("gst-price-display").innerHTML = "$" + (tempPrice*GST).toFixed(2);
+        document.getElementById("total-price-display").innerHTML = "$" + ((tempPrice + (tempPrice*QST) + (tempPrice*GST))).toFixed(2);
     }
     
     function calculatePriceChicken(){
         var priceChicken = (totalProductInCart[2].value)*6.99
         tempPrice += priceChicken; 
-        document.getElementById("temp-price-display").innerHTML = "$" + tempPrice;
-        document.getElementById("qst-price-display").innerHTML = "$" + tempPrice*QST;
-        document.getElementById("gst-price-display").innerHTML = "$" + tempPrice*GST;
-        document.getElementById("total-price-display").innerHTML = "$" + (tempPrice + (tempPrice*QST) + (tempPrice*GST));
+        document.getElementById("temp-price-display").innerHTML = "$" + (tempPrice).toFixed(2);
+        document.getElementById("qst-price-display").innerHTML ="$" + (tempPrice*QST).toFixed(2);
+        document.getElementById("gst-price-display").innerHTML = "$" + (tempPrice*GST).toFixed(2);
+        document.getElementById("total-price-display").innerHTML = "$" +((tempPrice + (tempPrice*QST) + (tempPrice*GST))).toFixed(2);
     }
 
     calculatePriceBeef();

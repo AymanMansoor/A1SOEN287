80487578ac74ca6346ac8d94b923241376ad46f3
s-vali <91510546+s-vali@users.noreply.github.com>
2021-11-30 00:42:57 -0500

    Add files via upload

--- P5Signingin.php
+++ P5Signingin.php
@@ -5,58 +5,56 @@ session_start();
 //Variables and constants
 $errorsUsername = 0;
 $errorsPassword = 0;
-$NBOFUSERS = 2;
 
 if(isset($_POST['login-btn'])){ //All action starts when submit button has been pressed
+    $_SESSION['submitLogin'] = true;
 
     /*Obtain login info from fields*/
     $loginInfo = array("username"=>$_POST['email'],"password"=>$_POST['pswrd']);
 
+    /*Count the number of user elements in xml file*/
+    $doc = new DOMDocument(); //Create a new DOMDocument
+    $doc->load('usersFile.xml'); //load the xml file to the document
+    $user = $doc->getElementsByTagName('user')->length; //creates the XML document and determines number of 'user' elements
+    $doc->saveXML();
+
     /*Validate that info correlates to an already registered account from xml file*/
     $xmldata = simplexml_load_file('usersFile.xml'); //interprets xml file into an object
-    for($i = 0; $i < $NBOFUSERS; $i++){  
+    for($i = 0; $i < $user; $i++){  
         $xmlUsername = $xmldata->user[$i]->email; //Retrieves user info from xml file
         $xmlPassword = $xmldata->user[$i]->password;
-        //echo $xmlUsername;
-        //echo $xmlPassword;
 
-        if($loginInfo["username"] ===  $xmlUsername) 
+        if($loginInfo["username"] ===  trim($xmlUsername)) 
         {
-            if($loginInfo["password"] === $xmlPassword) 
+            if($loginInfo["password"] === trim($xmlPassword)) 
             {
                 if($xmlUsername == "Admin" && $xmlPassword == "Admin")
                 {
                     /*You have successfully signed in as Admin. You are given internal privaliges*/
-                    $_SESSION['email'] = $xmlUsername; //Creates session variables for the Admin currently logged in
-                    $_SESSION['pswrd'] = $xmlPassword;
+                    $_SESSION['email'] = $loginInfo["username"]; //Creates session variables for the Admin currently logged in
+                    $_SESSION['pswrd'] = $loginInfo["password"];
                     header("Location: P11OrderList.php"); //redirects to the backstore pages
                     exit();
                 } else 
                     {
-                        /*You have successfully signed in */
-                        //echo "<script type="text/javascript"> alert("Successfully logged in"); </script>"; 
-                        $_SESSION['email'] = $xmlUsername; //Creates session variables for the user currently logged in
-                        $_SESSION['pswrd'] = $xmlPassword;
+                        /*You have successfully signed in */ 
+                        $_SESSION['email'] = $loginInfo["username"]; //Creates session variables for the user currently logged in
+                        $_SESSION['pswrd'] = $loginInfo["password"];
                         header("Location: P1Menu.php"); //redirects the page to the menu
                         exit(); 
                     } 
             } else 
                 {
-                    $errorsPassword = $errorsPassword+1; //error count to determine the type of alert message
+                    $_SESSION['wrongPassword'] = true;
+                    //header("Location: P5LoginError.php"); 
                 }
         } else 
             {
-                $errorsUsername = $errorsUsername+1;
+                $_SESSION['wrongEmail'] = true;
+                //header("Location: P5LoginError.php"); //logical issue with this when iterating in the for loop to find the email match
             }
-    }
+    } //end of for loop
 
-    /*Username and password error alerts*/
-    if($errorsUsername >= 1){
-        echo "This user does not exist"; //need to fix this to be an actual alert
-    } else if($errorsPassword >= 1) {
-        echo "The password inputted is incorrect";
-    }
-    
     /*Keep the user logged in while navigating */
     if(isset($_SESSION["loginInfo"]))
     //Saving the username and password as cookies 
@@ -69,7 +67,7 @@ if(isset($_POST['login-btn'])){ //All action starts when submit button has been 
         setcookie("password", $xmlPassword, time()+(10*365*24*60*60));
 
         $_SESSION["loginInfo"] =  $loginInfo;
-    }
+    }      
     else
     {
         if(isset($_COOKIE["username"]))
